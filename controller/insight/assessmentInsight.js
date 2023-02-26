00377c9f579ff46bb6cbf7374194b65fa6274ffd
akashpatelknit <cotsec14@gmail.com>
2023-02-26 16:22:24 +0530

    recommendation added for all users

--- controller/insight/assessmentInsight.js
+++ controller/insight/assessmentInsight.js
@@ -5,28 +5,25 @@ const { getRecommendation } = require('../../functions/recommendation');
 const { getImprovement } = require('../../functions/user_improvement');
 const getassessmentData = async () => {
 
-    console.log("fetching data");
     const res = await axios.get('https://on-the-move.onrender.com/api/v1/collection?name=monthlyAssessment')
-    // console.log(res.data);
+    
     return res.data
 }
 const getData = async () => {
 
-    console.log("fetching data");
     const res = await axios.get('https://on-the-move.onrender.com/api/v1/client/?batch=SHRED')
-    console.log(res.data);
     return res.data
 }
-getData();
+// getData();
 router.post("/", async (req, res) => {
     
     const user_assessment_data =await getassessmentData();
-    console.log("data fetched");
+   
     const user_improvement_allMonth = getImprovement(user_assessment_data);
     // filter latest month and pass to getRecommendation function
     const user_recommendation_latestmonth = await getRecommendation(user_improvement_allMonth);
 
-    res.json(user_improvement_allMonth);
+    res.json(user_recommendation_latestmonth);
 
 
 
