9fccf120abf6b743416ee693054993416b918ec2
Pranav Yadav <pranav-toptal@xendit.co>
2023-03-06 12:04:20 +0530

    health check

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 const port=process.env.PORT || 3000;
 mongoose.set('strictQuery', false);
+const router = express.Router();
 
 const assessmentInsightsController = require('./controller/insight/assessmentInsightController')
 const consistencyInsightsController = require('./controller/insight/consistencyInsightController')
@@ -15,6 +16,11 @@ app.use('/insight/assessment',assessmentInsightsController)
 app.use('/insight/consistency',consistencyInsightsController)
 app.use('/insight/intensity',averageintensityController)
 app.use('/workout',workoutController)
+
+app.get("/health", async (req, res) => {
+    res.json("service is running");
+
+})
 
 app.listen(port, async () => {
     await initDB()
