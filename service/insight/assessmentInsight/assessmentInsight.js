4a5a69c8c9a24108b85ca8cbc984a1f02509f9f1
akashpatelknit <cotsec14@gmail.com>
2023-03-07 20:36:33 +0530

    access directly dababase and check workout detail for one user

--- service/insight/assessmentInsight/assessmentInsight.js
+++ service/insight/assessmentInsight/assessmentInsight.js
@@ -7,11 +7,12 @@ const { getImprovement } = require("./user_improvement");
 // calc all users
 // new func -> 1 user
 // call new func 1  by 1 for all users
-const getAssessmentInsight = async () => {
+const getAssessmentInsight = async (title) => {
 
     const user_assessment_data =await getAll(DB_COLLECTION.ASSESSMENT);
     const user_improvement_allMonth = getImprovement(user_assessment_data); 
-    const user_recommendation_latestmonth = await getRecommendation(user_improvement_allMonth);
+    const improvement_one_user=user_improvement_allMonth.filter(r=>r.name==title)
+    const user_recommendation_latestmonth = await getRecommendation(improvement_one_user);
     return user_recommendation_latestmonth
 }
 
