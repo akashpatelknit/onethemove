26f4018d0b5c0efd847d42d1b16a0b5f81022cf2
Pranav Yadav <pranav-toptal@xendit.co>
2023-03-10 12:56:00 +0530

    main function for user consistency

--- controller/insight/consistencyInsightController.js
+++ controller/insight/consistencyInsightController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getAssessmentInsight } = require('../../service/insight/assessmentInsight/assessmentInsight');
+const { getUserConsistency } = require('../../service/insight/consistency/getconsistency');
 const { getWorkoutAttendance } = require('../../service/insight/consistency/insight_attendance');
 const router = express.Router();
 
@@ -7,6 +8,14 @@ const router = express.Router();
 router.get("/workout/attendance", async (req, res) => {
     
     const insights = await getWorkoutAttendance()
+    res.json(insights);
+
+})
+
+router.get("/user", async (req, res) => {
+    
+    const memberCode = req.query.code
+    const insights = await getUserConsistency(memberCode)
     res.json(insights);
 
 })
--- service/insight/consistency/getconsistency.js
+++ service/insight/consistency/getconsistency.js
@@ -3,17 +3,45 @@ const { DB_COLLECTION } = require("../../../db/dbDetails");
 const { getAttendanceData, getCollectionData } = require("../../axiosService");
 
 
+const getUserConsistency = async(memberCode) => {
 
-let getOverallConsistency = async(attendanceData,membercode)=>{
-   
-    const memberattendance= attendanceData.filter(a=>a.member_code==membercode);
-    return memberattendance.length;
+  console.log("Calc consistency stats for", memberCode);
+
+
+
+  const attendanceData = await getAll(DB_COLLECTION.ATTENDANCE, {
+    member_code: memberCode
+  })
+
+  // console.log(attendanceData.length);
+  const overallConsistency = getOverallConsistency(attendanceData, memberCode)
+
+  // adit ka first month , current month , count 
+
+  return {
+    member: memberCode,
+    consistency: {
+      overall: overallConsistency,
+      monthly: [
+        // { month, count}
+      ]
+    }
+  }
+
 }
+
+const getOverallConsistency = (attendanceData,membercode)=>{
+
+  const memberattendance= attendanceData.filter(a=>a.member_code==membercode);
+  return memberattendance.length;
+
+}
+
 let getmonthlyconsistency = async(attendanceData,membercode,month,year)=>{
   const monthly_member_attendance=attendanceData.filter(a=>(a.member_code==membercode && a.on.Month==month && a.on.Year==year) );
   return monthly_member_attendance.length;
 }
+
 module.exports={
-    getOverallConsistency,
-    getmonthlyconsistency
+    getUserConsistency
 }
