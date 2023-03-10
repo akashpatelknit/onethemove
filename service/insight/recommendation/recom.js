1b1bcd700a44f3ae2cf98ffafcd171a7882bc20a
Pranav Yadav <pranav-toptal@xendit.co>
2023-03-10 12:36:32 +0530

    per user per mvmt pseudo code

--- service/insight/recommendation/recom.js
+++ service/insight/recommendation/recom.js
@@ -4,45 +4,67 @@ const { DB_COLLECTION } = require('../../../db/dbDetails');
 const getRecom = async () => {
   const workoutSummary = await getAll(DB_COLLECTION.WORKOUTSUMMARY);
 
-  const users_with_data = workoutSummary.filter((r) => r.data != undefined);
+  const workoutData = workoutSummary.filter((r) => r.data != undefined);
 
-  const unique_user_name = [...new Set(users_with_data.map((r) => r.name))];
+  const unique_user_name = [...new Set(workoutData.map((r) => r.name))];
 
   const workoutMovementSummary=[]
 
-  unique_user_name.map((r) => {
+  unique_user_name.map((name) => {
 
-  const user_all_day_data = users_with_data.filter((name) => name.name == r);
+  const user_all_day_data = workoutData.filter((wd) => wd.name == name);
 
-  const movement_array = [];
-  user_all_day_data.map((data) => {
+  
+  const perMovementHistory = []
+    // { code: , history: [{}]}
+  user_all_day_data.map((data) => { // 1 day data
         
-        for (let key in data.data) {
-          if (
-            key.includes('rpe') ||
-            key.includes('reps') ||
-            key.includes('weight')
-          ) {
-           
-            var obj={}
-            obj[key]=data.data[key]
-            movement_array.push(
-            obj
-            )
-          }
-        }
+    const movement_array = [];
+    for (let key in data.data) {
+      if (
+        (
+          key.includes('-rpe') ||
+          key.includes('-reps') ||
+          key.includes('-weight') 
+        ) 
+        &&
+        !(
+          key.includes("STR") ||
+          key.includes("strength") 
+        )
+      ) {
+        
+        var obj={}
+        obj[key]=data.data[key]
+        movement_array.push(
+        obj
+        )
+      }
+    }
+    console.log(movement_array);
+
+    // unique movements find
+    // const mvmtCode = mvmtData.split("-")[0]
+    // PU, P, CF, CP
+
+    // perMovementHistory PU -> history push {pu-reps pu -rpe} 
+    // const isHistoryPresent = perMovementHistory.filter(mh => mh.code == mvmtCode).length
+
+  //   if (isHistoryPresent) {
+  //     perMovementHistory.filter(mh => mh.code == mvmtCode)[0].history
+  //   } // else history
+
+
   });
   
-  // const key=[]
-  // movement_array.map(key_name=>{
-  // key.push(Object.keys(key_name)[0])
-  // })
-  // let unique_key = [...new Set(key)]
 
-     workoutMovementSummary.push({
-      name:r,
-      movements:movement_array
-     })
+
+  
+
+  workoutMovementSummary.push({
+    name:name,
+    movementHistory: perMovementHistory
+    })
   });
 
   return workoutMovementSummary;
