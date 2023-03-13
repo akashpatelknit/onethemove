256ad435efe588f1d2a423fcc554e18593c48d23
Pranav Yadav <pranav-toptal@xendit.co>
2023-03-13 12:55:41 +0530

    code review

--- service/insight/consistency/getconsistency.js
+++ service/insight/consistency/getconsistency.js
@@ -19,8 +19,6 @@ const getUserConsistency = async(memberCode) => {
 
   console.log("Calc consistency stats for", memberCode);
 
-
-
   const attendanceData = await getAll(DB_COLLECTION.ATTENDANCE, {
     member_code: memberCode
   })
@@ -29,12 +27,13 @@ const getUserConsistency = async(memberCode) => {
   const overallConsistency = getOverallConsistency(attendanceData, memberCode)
 
   // adit ka first month , current month , count 
+  const monthlyConsistency = getMonthlyConsistency(attendanceData, memberCode)
 
   return {
     member: memberCode,
     consistency: {
       overall: overallConsistency,
-      monthly: data
+      monthly: monthlyConsistency
     }
   }
 
@@ -47,29 +46,43 @@ const getOverallConsistency = (attendanceData,membercode)=>{
 
 }
 
-let getmonthlyconsistency = async(attendanceData,membercode)=>{
-  let monthly_member_attendance=attendanceData.filter(a=>(a.member_code==membercode) );
-       monthly_member_attendance.sort(customsort);
-       let count=0;
-       let data=[];
-       for(let i=1;i<monthly_member_attendance.length;i++){
-           
-            let curr_mon=monthly_member_attendance[i].on.month;
-            let prev_mon=monthly_member_attendance[i-1].on.month;
-            let curr_year=monthly_member_attendance[i].on.year;
-            let prev_year=monthly_member_attendance[i-1].on.year;
-            if(curr_mon-prev_mon==0 && curr_year-prev_year==0){
-              count++;
-            }
-            else{
-                   let dummy={
-                    month:prev_mon,
-                    count:count
-                   }
-                   data .push(dummy);
-                   count=0;
-            }
-       }
+let getMonthlyConsistency = (attendanceData, membercode)=>{
+  let memberAttendance=attendanceData.filter(a=>(a.member_code==membercode) );
+
+  memberAttendance.sort(customsort);
+  let count=0;
+  let data=[];
+
+  // calc first month, current month 
+  // 10-22 , 3-23
+
+  // monthly attendance array
+  // 10-22, 11-22, 12-22, 1-23, 2-23, 3-23
+  // filter -> length -> count 
+
+  // sort data
+  // 
+  // month's non - exist => 0 
+
+  for(let i=1;i<memberAttendance.length;i++){
+      
+      let curr_mon=memberAttendance[i].on.month;
+      let prev_mon=memberAttendance[i-1].on.month;
+      let curr_year=memberAttendance[i].on.year;
+      let prev_year=memberAttendance[i-1].on.year;
+      if(curr_mon-prev_mon==0 && curr_year-prev_year==0){
+        count++;
+      }
+      else{
+              let dummy={
+              month:prev_mon,
+              // year: ,
+              count:count
+              }
+              data .push(dummy);
+              count=0;
+      }
+  }
   return data;
 }
 
